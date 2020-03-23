1f4ae225d98825d7360c0580d677ef5a77fc88f0
Abhishek Sharma <sharmaash876@gmail.com>
2020-03-23 18:15:13 +0530

    milestone 2

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const https = require('https');
 
+
 const app = express();
 
+
 app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 app.use(express.static("public"));
+
 
 app.get("/" , function(req , res)
 {
   https.get("https://covid19.mathdro.id/api/" , function(response)
-{
+  {
   response.on("data" , function(data)
+  {
+      var apidata = JSON.parse(data);
+      const deaths = apidata.deaths.value;
+      const treated = apidata.recovered.value;
+      const infected = apidata.confirmed.value;
+      res.render("layout" , { deaths:deaths , treated:treated , infected:infected})
+  })
+  });
+});
+
+app.post("/" , function(req , res)
 {
-  const apidata = JSON.parse(data);
-  console.log(apidata);
-})
+
 })
 
-  res.render("layout")
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
